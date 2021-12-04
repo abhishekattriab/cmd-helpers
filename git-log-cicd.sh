f98b53f3a13f98fe1116de23b4e15f44a908b819
Abhishek Attri <attriab@vmware.com>
2021-12-04 20:40:39 +0530

    Added utility for git commit short hash and commit count

---
+++ git-log-cicd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+touch git-log-cicd
+
+echo "git log --format=%h -1 ." >> git-log-cicd
+
+echo "git rev-list --count $(git log --format=%h -1 .)" >> git-log-cicd
+
+chmod +x git-log-cicd 
+
+mv git-log-cicd /usr/local/bin/
