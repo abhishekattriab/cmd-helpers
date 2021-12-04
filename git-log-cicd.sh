305b6d591a8d961bb972c13abcec5364b5a8bf68
abhishekattriab <60079199+abhishekattriab@users.noreply.github.com>
2021-12-04 20:44:51 +0530

    Update git-log format cmd

--- git-log-cicd.sh
+++ git-log-cicd.sh
@@ -2,7 +2,7 @@
 
 touch git-log-cicd
 
-echo "git log --format=%h -1 ." >> git-log-cicd
+echo "echo $(git log --format=%h -1 .)" >> git-log-cicd
 
 echo "git rev-list --count $(git log --format=%h -1 .)" >> git-log-cicd
 
