5c586b69b56578e49d66be90a8ebc7f4a01a1e34
Abhishek Attri <attriab@vmware.com>
2021-12-04 13:36:41 +0530

    Updated utility

--- git-master.sh
+++ git-master.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-#stash local changes
-git stash
+touch git-master
 
-# delete local temporary utility branch if checked out already
-git branch -D git-master-util
+echo "# stash local changes" >> git-master
+echo "git stash" >> git-master
 
-# checkout new temporary util branch locally  
-git checkout -b git-master-util
+echo "# delete local temporary utility branch if checked out already" >> git-master
+echo "git branch -D git-master-util" >> git-master
 
-# delete master branch locally
-git branch -D master
+echo "# checkout new temporary util branch locally" >> git-master
+echo "git checkout -b git-master-util" >> git-master
 
-# checkout master from remote
-git checkout master
+echo "# delete master branch locally" >> git-master
+echo "git branch -D master" >> git-master
 
-# pull latest changes from master
-git pull --rebase
+echo "# checkout master from remote" >> git-master
+echo "git checkout master" >> git-master
 
-echo 'done'
+echo "# pull latest changes from master" >> git-master
+echo "git pull --rebase" >> git-master
+
+echo "echo 'done'" >> git-master
+
+chmod +x git-master
+mv git-master /usr/local/bin/
